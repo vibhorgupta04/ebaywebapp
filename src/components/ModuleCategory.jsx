d4b8c7e081460f4770e5bb59da62686b4222797c
Vibhor <vibhorgupta04@gmail.com>
2024-05-19 16:31:05 +0530

    added category images

--- src/components/ModuleCategory.jsx
+++ src/components/ModuleCategory.jsx
@@ -22,13 +22,119 @@ const ModuleCategory = ({ categoriesData }) => {
           >
             {categoriesData?.map((category) => (
               <SwiperSlide key={`category-${category}`}>
-                <div
-                  className="w-full md:w-fit flex flex-col justify-center items-center cursor-pointer"
-                >
+                <div className="w-full md:w-fit flex flex-col justify-center items-center cursor-pointer">
                   <Link
                     to={`/products/categoryId/${category}`}
                     className="flex flex-col items-center"
                   >
+                    {category === 'smartphones' && (
+                      <div>
+                        <img
+                          src="/assets/Category-images/francesco-1bBCtUAUMFI-unsplash.jpg"
+                          alt="logo"
+                          className="w-40 h-40 rounded md:w-40 md:h-40 lg:w-60 lg:h-60 object-cover"
+                        />
+                      </div>
+                    )}
+                    {category === 'laptops' && (
+                      <div>
+                        <img
+                          src="/assets/Category-images/clement-helardot-95YRwf6CNw8-unsplash.jpg"
+                          alt="logo"
+                          className="w-40 h-40 rounded md:w-40 md:h-40 lg:w-60 lg:h-60 object-cover"
+                        />
+                      </div>
+                    )}
+                    {category === 'fragrances' && (
+                      <div>
+                        <img
+                          src="/assets/Category-images/jane-palash-eqDo7KG13vc-unsplash.jpg"
+                          alt="logo"
+                          className="w-40 h-40 rounded md:w-40 md:h-40 lg:w-60 lg:h-60 object-cover"
+                        />
+                      </div>
+                    )}
+                    {category === 'groceries' && (
+                      <div>
+                        <img
+                          src="/assets/Category-images/maddi-bazzocco-Hz4FAtKSLKo-unsplash.jpg"
+                          alt="logo"
+                          className="w-40 h-40 rounded md:w-40 md:h-40 lg:w-60 lg:h-60 object-cover"
+                        />
+                      </div>
+                    )}
+                    {category === 'skincare' && (
+                      <div>
+                        <img
+                          src="/assets/Category-images/michela-ampolo-7tDGb3HrITg-unsplash.jpg"
+                          alt="logo"
+                          className="w-40 h-40 rounded md:w-40 md:h-40 lg:w-60 lg:h-60 object-cover"
+                        />
+                      </div>
+                    )}
+                    {category === 'furniture' && (
+                      <div>
+                        <img
+                          src="/assets/Category-images/curology-6CJg-fOTYs4-unsplash.jpg"
+                          alt="logo"
+                          className="w-40 h-40 rounded md:w-40 md:h-40 lg:w-60 lg:h-60 object-cover"
+                        />
+                      </div>
+                    )}
+                    {category === 'tops' && (
+                      <div>
+                        <img
+                          src="/assets/Category-images/alyssa-strohmann-TS--uNw-JqE-unsplash.jpg"
+                          alt="logo"
+                          className="w-40 h-40 rounded md:w-40 md:h-40 lg:w-60 lg:h-60 object-cover"
+                        />
+                      </div>
+                    )}
+                    {category === 'home-decoration' && (
+                      <div>
+                        <img
+                          src="/assets/Category-images/cole-keister-ZN9LhLYCVko-unsplash.jpg"
+                          alt="logo"
+                          className="w-40 h-40 rounded md:w-40 md:h-40 lg:w-60 lg:h-60 object-cover"
+                        />
+                      </div>
+                    )}
+                    {category === 'womens-shoes' && (
+                      <div>
+                        <img
+                          src="/assets/Category-images/emily-pottiger-Zx76sbAndc0-unsplash.jpg"
+                          alt="logo"
+                          className="w-40 h-40 rounded md:w-40 md:h-40 lg:w-60 lg:h-60 object-cover"
+                        />
+                      </div>
+                    )}
+                    {category === 'womens-dresses' && (
+                      <div>
+                        <img
+                          src="/assets/Category-images/burgess-milner-OYYE4g-I5ZQ-unsplash.jpg"
+                          alt="logo"
+                          className="w-40 h-40 rounded md:w-40 md:h-40 lg:w-60 lg:h-60 object-cover"
+                        />
+                      </div>
+                    )}
+                    {category === 'mens-shirts' && (
+                      <div>
+                        <img
+                          src="/assets/Category-images/amir-foghani-3tD-_BXTUlw-unsplash.jpg"
+                          alt="logo"
+                          className="w-40 h-40 rounded md:w-40 md:h-40 lg:w-60 lg:h-60 object-cover"
+                        />
+                      </div>
+                    )}
+                    {category === 'mens-shoes' && (
+                      <div>
+                        <img
+                          src="/assets/Category-images/sahej-brar-U6rBOaaRNB0-unsplash.jpg"
+                          alt="logo"
+                          className="rounded w-40 h-40 lg:w-60 lg:h-60 object-cover"
+                        />
+                      </div>
+                    )}
                     {/* <div className="flex items-center bg-gray-300 rounded-full mx-auto">
                       {image ? (
                         <img
